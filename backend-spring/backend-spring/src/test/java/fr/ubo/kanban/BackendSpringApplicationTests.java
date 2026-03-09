e79e0b02cc8e7c4233269ba3421fe51b4d8492c5
Sanndobtg <Sanndobatengue@gmail.com>
2026-03-09 16:19:13 +0100

    chore : initialisation de spring boot structure du projet

---
+++ backend-spring/backend-spring/src/test/java/fr/ubo/kanban/BackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ubo.kanban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
