116bc0cdfab50a057942db7a90cf462ad0fb3d6f
Sanndobtg <Sanndobatengue@gmail.com>
2026-03-12 16:10:50 +0100

    chore: suppression fichiers Deno dupliqués à la racine

--- src/middleware/logger.ts
+++
@@ -1,8 +0,0 @@
-import { Context, Next } from "oak";
-
-export async function logger(ctx: Context, next: Next): Promise<void> {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  console.log(`${ctx.request.method} ${ctx.request.url} - ${ctx.response.status} (${ms}ms)`);
-}
