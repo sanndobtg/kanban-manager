1d06a34d1509ed4cbb6bbe458addbb5bf0faeae5
Olabiyi Pio ELEGBEDE <136569594+PioELG@users.noreply.github.com>
2026-03-12 13:58:56 +0100

    Update SecurityConfig.java

--- backend-spring/src/main/java/fr/ubo/kanban/common/security/SecurityConfig.java
+++ backend-spring/src/main/java/fr/ubo/kanban/common/security/SecurityConfig.java
@@ -1,27 +1,49 @@
 package fr.ubo.kanban.common.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwksUri;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        // Routes publiques → pas besoin de token
+                        .requestMatchers(HttpMethod.POST, "/api/utilisateurs").permitAll()
+                        // Tout le reste nécessite un token
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth -> oauth
+                        .jwt(jwt -> jwt.decoder(jwtDecoder()))
+                )
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // tout autoriser pour l'instant
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder
+                .withJwkSetUri(jwksUri)
+                .build();
     }
 }
